93c255afaac9e109c842bfd6bd2089c6b8b5b976
gordonmoore926 <ggmoore@princeton.edu>
2017-12-17 08:43:50 -0500

    some commenting

--- quikvid/Controllers/MainTabBarController.swift
+++ quikvid/Controllers/MainTabBarController.swift
@@ -5,6 +5,11 @@
 //  Created by Alexander Lewis on 12/16/17.
 //  Copyright © 2017 Alexander Lewis. All rights reserved.
 //
+//  MainTabBarController helps to implement the photo tab bar item as a button feature
+//  1. Subclasses UITabBarController with our own custom class
+//  2. Implements the UITabBarController Delegate
+//  3. Asks its delegate whether or not it should present the view controller
+//     that belongs to the tab bar item that the user just tapped
 
 import UIKit
 
@@ -21,14 +26,21 @@ class MainTabBarController: UITabBarController {
             print("handle image")
         }
         
+        // Set MainTabBarController as the delegate of its tab bar
         delegate = self
+        // Set the tab bar's unselectedItemTintColor from the default of gray to black
         tabBar.unselectedItemTintColor = .black
     }
 }
 
 extension MainTabBarController: UITabBarControllerDelegate {
+    // Returns a Bool that determines if the tab bar will present the corresponding
+    // UIViewController that the use has selected. True --> behave as usual.
+    // False --> view controller will not be displayed
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) ->
     Bool {
+        // Photo taking tab is tagged 1
+        // If the view controllers tag is the photo taking tag, return false
         if viewController.tabBarItem.tag == 1 {
             // present photo taking action sheet
             photoHelper.presentActionSheet(from: self)
